e204e6afeda16adc1919ddfaf15d32cecf257d8a
Wilson Agrela <wilson-andre100@hotmail.com>
2021-05-18 09:21:38 +0100

    add icon, functions append numbers and operations

--- javascript/app.js
+++ javascript/app.js
@@ -2,18 +2,32 @@ const previousOperand = document.querySelector(".previous-operand-JS")
 const currentOperand = document.querySelector(".current-operand-JS")
 const numeros = document.querySelectorAll("[data-numero]")
 const operacoes = document.querySelectorAll("[data-operacao]")
+const AC = document.querySelector("[data-AC]")
 
+let contas = operacao => {
+    //implement
+}
 
 numeros.forEach(numero => {
     numero.addEventListener("click", () =>{
-        
-        currentOperand.innerHTML = numero.innerHTML
+
+        if(numero.innerHTML === '.' && currentOperand.innerHTML.includes('.')) return
+
+        currentOperand.innerHTML = currentOperand.innerHTML.toString() + numero.innerHTML.toString()  //precisamos de converter para string, pois se fosse um numero o javascript ia tentar somar os dois 
     })
 })
 
 operacoes.forEach(operacao => {
     operacao.addEventListener("click", () =>{
+        if(currentOperand.innerHTML === '') return
 
-        currentOperand.innerHTML = operacao.innerHTML
+        currentOperand.innerHTML = currentOperand.innerHTML.toString() + operacao.innerHTML.toString()
+        previousOperand.innerHTML =  currentOperand.innerHTML
+        currentOperand.innerHTML = ""
     })
+})
+
+AC.addEventListener("click", () =>{
+    currentOperand.innerHTML = ""
+    previousOperand.innerHTML = ""
 })
